c35f11dd48c6475984e79a51e02a44d680c356d7
глеб <128576701+glebyushancev1@users.noreply.github.com>
2023-04-05 09:49:53 +0300

    Add files via upload

---
+++ лр 2 Ющанцев/WpfLR2/ClsZNAK.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfLR2
+{
+    internal class ClsZNAK
+    {
+        //поля
+        private string fam;
+        private string name;
+        private DateTime date;
+        private string znak;
+
+        // свойства для доступа к полям
+        public string Fam
+        {
+            get { return fam; }//возвращает значение поля
+            set { fam = value; } //устанавливает значение поля
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Znak
+        {
+            get { return znak; }
+            set { znak = value; }
+        }
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        //методы 
+        //конструктор без параметров задает значения полей по умолчанию
+        public ClsZNAK()
+        {
+            fam = "";
+            name = "";
+            znak = "";
+            date = new DateTime(2001, 01, 01);
+        }
+        //конструктор с параметрами
+        public ClsZNAK(string f, DateTime d, string z,
+            string n)
+        {
+            fam = f;
+            date = d;
+            name = n;
+            znak = z;
+        }
+        //метод вывода на экран
+        public string PrintToScreen()
+        {
+            string line = $"{fam} {name}" + "\n";
+            line += $"Родился {date}" + "\n";
+            line += $"Год поступления {znak}";
+            return line;
+        }
+        public void SaveToFile(string filename)
+        {
+            StreamWriter streamWriter = new StreamWriter(filename, true, Encoding.Default);
+            streamWriter.WriteLine("Личные данные человека");
+            streamWriter.WriteLine($"Фамилия: {fam}");
+            streamWriter.WriteLine($"Имя: {name}");
+            streamWriter.WriteLine($"Дата рождения: {date}");
+            streamWriter.WriteLine($"должность: {znak}");
+            streamWriter.Close();
+        }
+    }
+}
---
+++ лр 2 Ющанцев/WpfLR2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLR2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        ClsZNAK chel = new ClsZNAK();
+
+        private void BtnSolve_Click(object sender, RoutedEventArgs e)
+        {
+            chel.Fam = TxtInputFam.Text;
+            chel.Name = TxtInputName.Text;
+            chel.Znak = TxtInputZnak.Text;
+            chel.Date = (DateTime)TxtInputDate.SelectedDate;
+            LstResult.Items.Clear();
+            LstResult.Items.Add(chel.PrintToScreen());
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == false)
+                return;
+            chel.SaveToFile(saveFileDialog.FileName);
+
+        }
+    }
+}
